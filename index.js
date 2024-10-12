73e8d94797841428720b129697de51f025b9f824
Siddha Lakshmi Prasanna <siddhalakshmiprasanna2002@gmail.com>
2024-10-12 15:38:57 +0530

    Done small changes in code

---
+++ index.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5];
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2==1){
+        console.log("Odd numbers : " +arr[i]);
+    }else{
+        console.log("Even numbers : " +arr[i]);
+    }
+}
